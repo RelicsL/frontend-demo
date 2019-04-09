f1f9caf6d3eec679ffc5a56e2f8589d2da73da11
relics <1281336591@qq.com>
2019-04-09 08:09:52 +0800

    项目重构

---
+++ src/Containers/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.scss';
+import { Layout } from 'antd';
+class App extends Component {
+  render() {
+    const { Header , Content , Footer } = Layout
+    return (
+      <div className="App">
+        <Layout className="main-layout">
+          <Header className="main-header">
+            <div className="logo">
+              <img src="http://s2.bai.itc.cn/r/c/themes_v1310711076/global/cr/0001/i/head-cr.gif"/>
+            </div>
+          </Header>
+          <Content className="main-content">
+
+          </Content>
+          <Footer className="main-footer"></Footer>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
